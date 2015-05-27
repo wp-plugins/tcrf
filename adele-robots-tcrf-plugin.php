70f623fa13949571cd6b479857574e02d79ba9b9
The Client Relations Factory <The Client Relations Factory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-27 07:51:48 +0000

    Deprecated warning solved
    git-svn-id: https://plugins.svn.wordpress.org/tcrf/trunk@1168286 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adele-robots-tcrf-plugin.php
+++ adele-robots-tcrf-plugin.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 /**
  * Plugin Name: The Client Relations Factory
  * Plugin URI: http://www.theclientrelationsfactory.com/
